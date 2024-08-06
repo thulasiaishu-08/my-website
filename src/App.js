35f87862bea23ff02454bf2bcf7c675153a2e7df
thulasiaishu-08 <thulasiaishwaryaa@gmail.com>
2024-08-06 13:58:21 +0530

    about

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from './components/layouts/Home';
 import "./assets/css/style.css";
+import About from './components/layouts/About';
 
 function App() {
   return (
    <BrowserRouter>
     <Routes>
       <Route path='/' element= {<Home/>}></Route>
+      <Route path='/about' element={<About/>}></Route>
     </Routes>
    </BrowserRouter>
   );
