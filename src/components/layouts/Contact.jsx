8b497f972888f3c1270c19b48e4355a4641c11ef
thulasiaishu-08 <thulasiaishwaryaa@gmail.com>
2024-08-07 13:42:45 +0530

    Contact page

--- src/components/layouts/Contact.jsx
+++ src/components/layouts/Contact.jsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
 import Footer from "../Footer";
 import Header from "../Header";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -11,21 +11,34 @@ function Contact(){
 
         <section className="pagebg">
             <Container>
-                <h2>Services</h2>
+                <h2>Contact</h2>
             </Container>
         </section>
 
-        <section>
+        <section className="contact">
             <Container>
+                <h3>Reach Us</h3>
                 <Row>
                     <Col md={6}>
-                        <div>
-                            <h3>Get in Touch with TechWave Solutions</h3>
+                        <div className="form-contact">
+                            <h2>Get in Touch with TechWave Solutions</h2>
+                                <Form>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                        <Form.Control type="name" placeholder="Name" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                                        <Form.Control type="email" placeholder="Email" />
+                                    </Form.Group>
+                                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                        <Form.Control as="textarea" placeholder="Message" rows={3} />
+                                    </Form.Group>
+                                    <Button type="submit">Send Message</Button>
+                                </Form>
                         </div>
                     </Col>
                     <Col md={6}>
-                        <div>
-                            <h3>Contact Info</h3>
+                        <div className="contact-content">
+                            <h2>Contact Info</h2>
                             <h6>Address</h6>
                             <p>123 Innovation Drive
                             Tech City, TC 12345</p>
@@ -45,6 +58,10 @@ function Contact(){
                 </Row>
             </Container>
         </section>
+
+        <div className="map">
+            <iframe frameborder="0" src="https://www.google.com/maps/embed/v1/place?q=123+Innovation+Drive+San+Francisco,+CA+94105&key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8"></iframe>
+        </div>
 
         <Footer/>
         </>
